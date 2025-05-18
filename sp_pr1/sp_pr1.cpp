b84d8cc184ab6e71ae08cf26c9c1d690f3a5b388
AndreyK108 <150450270+AndreyK108@users.noreply.github.com>
2025-05-18 23:47:12 +0300

    Добавьте файлы проекта.

---
+++ sp_pr1/sp_pr1.cpp
@@ -0,0 +1,10 @@
+﻿#include <Windows.h>
+#include "sp_pr1.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+    LPCTSTR lpszHelloText = GetMessageText();
+    LPCTSTR lpszHelloWndTitle = MESSAGE_TITLE;
+    MessageBox(NULL, lpszHelloText, lpszHelloWndTitle, MB_OK);
+    return 0;
+}
