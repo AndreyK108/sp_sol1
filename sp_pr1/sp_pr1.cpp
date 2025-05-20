dc247038260ca24775acb97ec76d1959855eacc5
AndreyK108 <150450270+AndreyK108@users.noreply.github.com>
2025-05-20 13:04:14 +0300

    Pr1 V2

--- sp_pr1/sp_pr1.cpp
+++ sp_pr1/sp_pr1.cpp
@@ -1,8 +1,9 @@
 ﻿#include <Windows.h> 
+#include "sp_pr1.h" 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
 {
-	LPCTSTR lpszHelloText = TEXT("Hello World Win32 GUI Application");
-	LPCTSTR lpszHelloWndTitle = TEXT("Окно сообщения");
+	LPCTSTR lpszHelloText = MESSAGE_TEXT;
+	LPCTSTR lpszHelloWndTitle = MESSAGE_TITLE;
 	MessageBox(NULL, lpszHelloText, lpszHelloWndTitle, MB_OK);
 	return 0;
 }
