24d13a535cdafba15decbb2ac2b30c86d3bb6e12
AndreyK108 <150450270+AndreyK108@users.noreply.github.com>
2025-05-20 13:28:18 +0300

    Pr1 V3

--- sp_pr1/sp_pr1.cpp
+++ sp_pr1/sp_pr1.cpp
@@ -1,9 +1,10 @@
-﻿#include <Windows.h> 
-#include "sp_pr1.h" 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+﻿#include <Windows.h>
+#include "sp_pr1.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE PrevInstance, LPSTR IpszCmdLine, int nCmdShow)
 {
-	LPCTSTR lpszHelloText = MESSAGE_TEXT;
-	LPCTSTR lpszHelloWndTitle = MESSAGE_TITLE;
-	MessageBox(NULL, lpszHelloText, lpszHelloWndTitle, MB_OK);
+	LPCTSTR LpszHelloText = GetMessageText();
+	LPCTSTR LpszHelloWndTitle = MESSAGE_TITLE;
+	MessageBox(NULL, LpszHelloText, LpszHelloWndTitle, MB_OK);
 	return 0;
 }
